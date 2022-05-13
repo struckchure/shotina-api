2f2b15633cb7503ff5b379a94fadd62bed0dedf5
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2022-05-14 00:21:18 +0100

    updates app middleware injection

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 import express from "express";
 
+import connect_to_database from "./config/db/db.js";
 import middlewares from "./middlewares/index.js";
 import router from "./routes/index.js";
 
 const app = express();
+
+// database
+connect_to_database();
 
 // middlewares
 middlewares.map((middleware) => app.use(middleware));
--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import logger from "morgan";
 
 export default [
-  logger("dev"),
   express.json(),
   express.urlencoded({ extended: false }),
+  logger("dev"),
 ];
